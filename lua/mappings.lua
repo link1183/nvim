dfe7971879268786f5cd160ccac885e172187b7e
link1183 <adrien.gunther@gmail.com>
2024-05-12 17:56:56 +0200

    minor changes

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -5,6 +5,15 @@ require "nvchad.mappings"
 local keymap = vim.keymap
 local M = {}
 
+M.general = {
+  n = {
+    ["<C-h>"] = { "<cmd> TmuxNavigateLeft <CR>", "window left" },
+    ["<C-l>"] = { "<cmd> TmuxNavigateRight <CR>", "windows right" },
+    ["<C-j>"] = { "<cmd> TmuxNavigateDown <CR>", "windows down" },
+    ["<C-k>"] = { "<cmd> TmuxNavigateUp <CR>", "windows up" },
+  },
+}
+
 M.dap = {
   plugin = true,
   n = {
--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -314,17 +314,8 @@ local plugins = {
     end,
   },
   {
-    "dreamsofcode-io/ChatGPT.nvim",
-    event = "VeryLazy",
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope.nvim",
-    },
-    config = function()
-      require("chatgpt").setup {
-      }
-    end,
+    "christoomey/vim-tmux-navigator",
+    lazy = false,
   },
 }
 return plugins
