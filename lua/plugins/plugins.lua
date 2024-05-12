c2d2b270f0643463712216ce9d186406e80e69ad
link1183 <adrien.gunther@gmail.com>
2024-05-12 18:47:24 +0200

    minor changes

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -321,9 +321,9 @@ local plugins = {
     "dreamsofcode-io/ChatGPT.nvim",
     event = "VeryLazy",
     config = function()
-      require("chatgpt").setup {
-        async_api_key_cmd = "echo pass show api/openai",
-      }
+      require("chatgpt").setup()({
+        async_api_key_cmd = "pass show api/openai",
+      })
     end,
     dependencies = {
       "MunifTanjim/nui.nvim",
