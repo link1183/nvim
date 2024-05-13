186f762ebbda9d0de01d58011ffde73d33d8b593
link1183 <adrien.gunther@gmail.com>
2024-05-13 23:57:10 +0200

    added some plugins

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -1,5 +1,8 @@
 local plugins = {
   {
+    "tpope/vim-fugitive",
+  },
+  {
     "folke/noice.nvim",
     event = "VeryLazy",
     config = function()
@@ -37,11 +40,7 @@ local plugins = {
       }
     end,
     dependencies = {
-      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
       "MunifTanjim/nui.nvim",
-      -- OPTIONAL:
-      --   `nvim-notify` is only needed, if you want to use the notification view.
-      --   If not available, we use `mini` as the fallback
       "rcarriga/nvim-notify",
     },
   },
